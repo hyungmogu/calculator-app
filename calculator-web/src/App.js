8f0b7492556da520b88bbb49bbdf00f6d4ce76fb
Hyungmo Gu <guhyungm7@gmail.com>
2020-01-06 16:45:22 -0700

    fix: change class to className 'React web app'

--- calculator-web/src/App.js
+++ calculator-web/src/App.js
@@ -14,27 +14,27 @@ class App extends Component {
     return (
       <div className="App">
         <main>
-            <div class="calculator">
-              <section class="display">{display}</section>
-              <section class="number-pad">
-                  <div class="btn btn--clear" onclick="clear()">Clear</div>
-                  <div class="btn btn--operand-plus" onclick="addOperator('+')">+</div>
-                  <div class="btn btn--number-7" onclick="addNumber('7')">7</div>
-                  <div class="btn btn--number-8" onclick="addNumber('8')">8</div>
-                  <div class="btn btn--number-9" onclick="addNumber('9')">9</div>
-                  <div class="btn btn--operand-minus" onclick="addOperator('-')">-</div>
-                  <div class="btn btn--number-4" onclick="addNumber('4')">4</div>
-                  <div class="btn btn--number-5" onclick="addNumber('5')">5</div>
-                  <div class="btn btn--number-6" onclick="addNumber('6')">6</div>
-                  <div class="btn btn--operand-division" onclick="addOperator('+')">÷</div>
-                  <div class="btn btn--number-1" onclick="addNumber('7')">7</div>
-                  <div class="btn btn--number-2" onclick="addNumber('8')">8</div>
-                  <div class="btn btn--number-3" onclick="addNumber('9')">9</div>
-                  <div class="btn btn--operand-product" onclick="addOperator('*')">x</div>
-                  <div class="btn btn--sign" onclick="toggleSign()">+/-</div>
-                  <div class="btn btn--decimal" onclick="addDecimal('.')">.</div>
-                  <div class="btn btn--number-0" onclick="addNumber('0')">0</div>
-                  <div class="btn btn--operand-equal" onclick="calculate()">=</div>
+            <div className="calculator">
+              <section className="display">{display}</section>
+              <section className="number-pad">
+                  <div className="btn btn--clear" onClick="clear()">Clear</div>
+                  <div className="btn btn--operand-plus" onClick="addOperator('+')">+</div>
+                  <div className="btn btn--number-7" onClick="addNumber('7')">7</div>
+                  <div className="btn btn--number-8" onClick="addNumber('8')">8</div>
+                  <div className="btn btn--number-9" onClick="addNumber('9')">9</div>
+                  <div className="btn btn--operand-minus" onClick="addOperator('-')">-</div>
+                  <div className="btn btn--number-4" onClick="addNumber('4')">4</div>
+                  <div className="btn btn--number-5" onClick="addNumber('5')">5</div>
+                  <div className="btn btn--number-6" onClick="addNumber('6')">6</div>
+                  <div className="btn btn--operand-division" onClick="addOperator('+')">÷</div>
+                  <div className="btn btn--number-1" onClick="addNumber('7')">7</div>
+                  <div className="btn btn--number-2" onClick="addNumber('8')">8</div>
+                  <div className="btn btn--number-3" onClick="addNumber('9')">9</div>
+                  <div className="btn btn--operand-product" onClick="addOperator('*')">x</div>
+                  <div className="btn btn--sign" onClick="toggleSign()">+/-</div>
+                  <div className="btn btn--decimal" onClick="addDecimal('.')">.</div>
+                  <div className="btn btn--number-0" onClick="addNumber('0')">0</div>
+                  <div className="btn btn--operand-equal" onClick="calculate()">=</div>
               </section>
           </div>
         </main>
