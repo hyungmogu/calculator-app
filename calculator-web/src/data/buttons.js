13a1a54db2f8d06eab223df715e85171af17c98f
Hyungmo Gu <guhyungm7@gmail.com>
2020-01-07 10:52:51 -0700

    feat: Add data for buttons 'React web app'

---
+++ calculator-web/src/data/buttons.js
@@ -0,0 +1,106 @@
+export const CalculatorButtons = [
+    {
+        id: "button-clear",
+        type: "clear",
+        label: "Clear"
+    },
+    {
+        id: "button-plus",
+        type: "operator",
+        label: "+",
+        value: "+"
+    },
+    {
+        id: "button-7",
+        type: "number",
+        label: "7",
+        value: "7"
+    },
+    {
+        id: "button-8",
+        type: "number",
+        label: "8",
+        value: "8"
+    },
+    {
+        id: "button-9",
+        type: "number",
+        label: "9",
+        value: "9"
+    },
+    {
+        id: "button-minus",
+        type: "operator",
+        label: "-",
+        value: "-"
+    },
+    {
+        id: "button-4",
+        type: "number",
+        label: "4",
+        value: "4"
+    },
+    {
+        id: "button-5",
+        type: "number",
+        label: "5",
+        value: "5"
+    },
+    {
+        id: "button-6",
+        type: "number",
+        label: "6",
+        value: "6"
+    },
+    {
+        id: "button-division",
+        type: "operator",
+        label: "÷",
+        value: "/"
+    },
+    {
+        id: "button-1",
+        type: "number",
+        label: "1",
+        value: "1"
+    },
+    {
+        id: "button-2",
+        type: "number",
+        label: "2",
+        value: "2"
+    },
+    {
+        id: "button-3",
+        type: "number",
+        label: "3",
+        value: "3"
+    },
+    {
+        id: "button-multiplication",
+        type: "operator",
+        label: "x",
+        value: "*"
+    },
+    {
+        id: "button-sign",
+        type: "sign",
+        label: "+/-"
+    },
+    {
+        id: "button-decimal",
+        type: "decimal",
+        label: "."
+    },
+    {
+        id: "button-0",
+        type: "number",
+        label: "0",
+        value: "0"
+    },
+    {
+        id: "button-calculate",
+        type: "calculate",
+        label: "="
+    }
+];
