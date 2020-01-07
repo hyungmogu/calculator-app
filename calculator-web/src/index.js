f47f2d593e1a97c0cee5499600fc4beb260798ec
Hyungmo Gu <guhyungm7@gmail.com>
2020-01-07 12:24:15 -0700

    refactor: Wrap app in provider in index.js 'React web app'

--- calculator-web/src/index.js
+++ calculator-web/src/index.js
@@ -2,9 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/style.css';
 import App from './App';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from './components/Context'
+
+
+ReactDOM.render(
+    <Provider>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
